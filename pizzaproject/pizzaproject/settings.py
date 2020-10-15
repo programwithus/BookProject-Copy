df636c16288bb27fb918521d8a6f9d05af68c06a
programwithus <learntocodewithus@gmail.com>
2020-10-14 22:33:48 -0400

    two

--- pizzaproject/pizzaproject/settings.py
+++ pizzaproject/pizzaproject/settings.py
@@ -124,4 +124,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+if DEBUG is True:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/static/'
+    STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+    ]
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = '/var/www/venv/BookProject-Copy/pizzaproject/static/'
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/venv/BookProject-Copy/pizzaproject/media/')
